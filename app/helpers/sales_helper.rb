811bccbc60f5eb979c279be21c92f9d66713b9b8
JCPenne <jackcpalmer@icloud.com>
2022-10-28 12:34:23 -0700

    add further helper logic to query for first current sale and show in front-end

--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -1,5 +1,12 @@
 module SalesHelper
   def active_sale?
-    true
+    Sale.active.any?
+  end
+
+  def current_sale
+    if active_sale?
+      @current_sale = Sale.where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current).first
+    end
+  
   end
 end
